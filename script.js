a2be14bfffdee373eba34254ca2ab260655cea16
M-Zain-924823 <mz924823@gmail.com>
2026-02-19 23:23:57 +0500

    add more functionality like remove and read buttons and fixed the grid

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ Book.prototype.addBookToLibrary = function(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = (status) ? "read" : "unread";
+    this.status = status;
     myLibrary.push(this);
 }
 
+// Remove this useless part
 let book1 = new Book();
 let book2 = new Book();
 let book3 = new Book();
@@ -27,3 +28,53 @@ book3.addBookToLibrary("SE", "zain", 100, true);
 book4.addBookToLibrary("SDA", "mataiba", 200, false);
 console.log(myLibrary);
 
+let main = document.querySelector("main");
+
+function display() {
+    if (myLibrary.length === 0) {
+        let card = document.createElement("div");
+        let h2 = document.createElement("h2");
+        let message = document.createElement("p");
+        card.classList.add("card");
+        card.style.textAlign = "center";
+        h2.textContent = "No Books Added Yet";
+        message.textContent = "Please press the + button to add books";
+        card.appendChild(h2);
+        card.appendChild(message);
+        main.appendChild(card);
+    } else 
+    myLibrary.forEach((book, index) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let title = document.createElement("h2");
+        let author = document.createElement("div");
+        let pages = document.createElement("div");
+        let btn = document.createElement("button");
+        let cross = document.createElement("button");
+        cross.textContent = "X";
+        cross.setAttribute("id", "cross");
+        cross.setAttribute("data-identifier", book.id);
+        cross.addEventListener("click", () => {
+            myLibrary.splice(index, 1);
+            main.textContent = "";
+            display();
+        });
+        title.textContent = book.title;
+        author.textContent = "Author: " + book.author;
+        pages.textContent = "Pages: " + book.pages;
+        btn.textContent = book.status ? "Read" : "UnRead";
+        btn.addEventListener("click", () => {
+            book.status = book.status ? false : true;
+            btn.textContent = book.status ? "Read" : "UnRead";
+            // toggle theme of card
+        });
+        card.appendChild(cross);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(btn);
+        main.appendChild(card);
+    });
+}
+
+display();
